957cde7da509294044e51c75a28ceb8cb8efc109
lbpinheiro <pinheiro.lucas@gmail.com>
2016-07-12 21:46:49 -0300

    inserted root (static) page

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.use(express.static(__dirname + '/public'));
+app.set('view engine', 'hjs');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/*', function (req, res) {
+  if (req.originalUrl === '/') {
+    res.sendFile('index.html');
+
+  }
+  res.send('Hello World! ' + req.originalUrl);
+  console.log(req);
 });
 
 app.listen(3000, function () {
